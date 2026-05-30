12c46eafdb86455b3418a3412336e96b96b2f372
doublekilla <dihmas12@gmail.com>
2026-05-31 06:35:48 +0700

    Delete 1.0 directory

--- 1.0/db_iae/resources/js/ziggy.js
+++
@@ -1,7 +0,0 @@
-import { route as ziggyRoute } from '../../vendor/tightenco/ziggy/dist/index.esm.js';
-
-// Make route available globally for all components
-// This is needed because ESM modules can't access implicit globals
-window.route = ziggyRoute;
-
-export { ziggyRoute as route };
--- 1.0/db_iaex/android/app/src/main/kotlin/com/example/db_iaex/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.db_iaex
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
